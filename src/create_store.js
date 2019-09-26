2c10d8d211043a3b5e018c1ba181732ef664e78d
Deev Pavel <deev@uchi.ru>
2019-09-26 23:51:29 +0300

    minimal redux

--- src/create_store.js
+++ src/create_store.js
@@ -1,9 +1,31 @@
-export default function createStore(reducer, initialState) {
+export default function createStore(reducer, initialState = {}) {
+  if (typeof reducer !== 'function') throw new Error('reducer must be function');
+
   let state = initialState;
+  const currentReducer = reducer;
+
+  const subscribers = [];
   return {
-    dispatch: action => {
-      state = reducer(state, action);
+    dispatch(action) {
+      state = currentReducer(state, action);
+
+      subscribers.forEach(subscriber => {
+        subscriber(state);
+      });
     },
+
     getState: () => state,
+
+    subscribe(cb) {
+      subscribers.push(cb);
+
+      return () => {
+        const index = subscribers.indexOf(cb);
+
+        if (index >= 0) {
+          subscribers.splice(index, 1);
+        }
+      };
+    },
   };
 }
