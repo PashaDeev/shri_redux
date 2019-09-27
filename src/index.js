b4e324f7897a54c4f174e7f3bec9482763a1b9c5
Deev Pavel <deev@uchi.ru>
2019-09-27 22:54:06 +0300

    import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import createStore from './create_store';
 import applyMiddleware from './apply_middleware';
 
-export default { createStore, applyMiddleware };
+// eslint-disable-next-line func-names
+(function(root) {
+  if (typeof exports === 'object' && typeof module === 'object') {
+    module.exports = { createStore, applyMiddleware };
+    // eslint-disable-next-line no-undef
+  } else if (typeof define === 'function' && define.amd) {
+    // eslint-disable-next-line no-undef
+    define([], func);
+  } else if (typeof exports === 'object') {
+    exports.redux = { createStore, applyMiddleware };
+  }
+  // eslint-disable-next-line no-param-reassign
+  root.redux = { createStore, applyMiddleware };
+  if (typeof window !== `undefined`) {
+    window.redux = { createStore, applyMiddleware };
+  }
+
+  if (typeof global !== `undefined`) {
+    global.redux = { createStore, applyMiddleware };
+  }
+})(this);
